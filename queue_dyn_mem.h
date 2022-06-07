84c2f678712522e9b6f2bd13f5fbcf18e4b8258c
StevenChou499 <stevenchou499@gmail.com>
2022-06-07 17:41:52 +0800

    Add queue initialization and destroy

--- queue_dyn_mem.h
+++ queue_dyn_mem.h
@@ -13,4 +13,40 @@ typedef struct {
     size_t head, tail;
 } queue_t;
 
+/**
+ * @brief allocate memory to initialize queue
+ * 
+ * @param q pointer to the queue itself
+ * @param s size of queue
+ */
+void queue_init(queue_t *q, size_t s)
+{
+    q->buffer = (uint8_t *)malloc(sizeof(uint8_t) * s);
+    q->size = s;
+    q->head = q->tail = 0;
+}
+
+/**
+ * @brief free the allocated memory in queue
+ * 
+ * @param q pointer to the queue itself
+ */
+void queue_destroy(queue_t *q)
+{
+    free(q->buffer);
+}
+
+/**
+ * @brief insert the elements in @buffer to queue
+ * 
+ * @param q pointer to the queue itself
+ * @param buffer 
+ * @param size 
+ */
+void queue_put(queue_t *q, uint8_t *buffer, size_t size)
+{
+
+}
+
+
 #endif
