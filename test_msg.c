bb688bc55786c53db1d1b862a73406ae63b70781
StevenChou499 <stevenchou499@gmail.com>
2022-06-14 00:38:20 +0800

    Complete message size implementation.

--- test_msg.c
+++ test_msg.c
@@ -8,6 +8,7 @@
 #define BUFFER_SIZE (getpagesize())
 #define NUM_THREADS (1)
 #define MESSAGES_PER_THREAD (getpagesize() * 2)
+#define SIZE_OF_MESSAGE 10ULL
 
 uint32_t comp(const void *elem1, const void *elem2)
 {
@@ -26,6 +27,9 @@ typedef struct {
     uint32_t num_threads;
 } rbuf_t;
 
+size_t in[65536];
+size_t out[65536];
+
 /**
  * @brief Get timestamp
  * @return timestamp now
@@ -41,57 +45,146 @@ static void *publisher_loop(void *arg)
 {
     rbuf_t *r = (rbuf_t *) arg;
     size_t i;
-    for (i = 0; i < r->messages_per_thread * r->num_threads; i++)
-        queue_put(&r->q, (uint8_t *) &i, sizeof(size_t));
-    return (void *) i;
+    size_t **publisher_ptr = malloc(sizeof(size_t *));
+    *publisher_ptr = in;
+    size_t full_put = (r->messages_per_thread * r->num_threads) / SIZE_OF_MESSAGE;
+    size_t remain_put = (r->messages_per_thread * r->num_threads) % SIZE_OF_MESSAGE;
+    for (i = 0; i < full_put; i++)
+        queue_put(&r->q, (uint8_t **) publisher_ptr, sizeof(size_t) * SIZE_OF_MESSAGE);
+    if(remain_put)
+        queue_put(&r->q, (uint8_t **) publisher_ptr, sizeof(size_t) * remain_put);
+    return (void *) (i * SIZE_OF_MESSAGE + remain_put);
 }
 
 static void *consumer_loop(void *arg)
 {
     rbuf_t *r = (rbuf_t *) arg;
     size_t i;
-    for (i = 0; i < r->messages_per_thread; i++) {
-        queue_get(&r->q, (uint8_t *) &i, sizeof(size_t));
-        printf("%ld\n", x);
-    }
-    return (void *) i;
+    size_t **consumer_ptr = malloc(sizeof(size_t *));
+    *consumer_ptr = out;
+    size_t full_get = (r->messages_per_thread) / SIZE_OF_MESSAGE;
+    size_t remain_get = (r->messages_per_thread) % SIZE_OF_MESSAGE;
+    for (i = 0; i < full_get; i++)
+        queue_get(&r->q, (uint8_t **) consumer_ptr, sizeof(size_t) * SIZE_OF_MESSAGE);
+    if(remain_get)
+        queue_get(&r->q, (uint8_t **) consumer_ptr, sizeof(size_t) * remain_get);
+    return (void *) (i * SIZE_OF_MESSAGE + remain_get);
 }
 
 int main(int argc, char *argv[])
 {
-    rbuf_t r;
-    r.num_threads = 1U;
-    // if (argc > 1)
-        // r.messages_per_thread = (uint32_t) atoi(argv[1]);
-    // else
-    r.messages_per_thread = 65536;
-    queue_init(&r.q, BUFFER_SIZE);
+    uint32_t time[100];
+    for (int i = 0; i < 100; i++) {
+        for(size_t i = 0; i < 65536ULL; i++) {
+            in[i] = i;
+            out[i] = 0ULL;
+        }
 
-    uint64_t start = get_time();
+        rbuf_t r;
+        r.num_threads = 1U;
+        if (argc > 1)
+            r.messages_per_thread = (uint32_t) atoi(argv[1]);
+        else
+            r.messages_per_thread = 65536;
+    
+        queue_init(&r.q, BUFFER_SIZE);
 
-    pthread_t publisher;
-    pthread_t consumer;
+        uint64_t start = get_time();
 
-    pthread_attr_t attr;
-    pthread_attr_init(&attr);
+        pthread_t publisher_th;
+        pthread_t consumer_th;
 
-    pthread_create(&publisher, &attr, &publisher_loop, (void *) &r);
+        pthread_attr_t attr;
+        pthread_attr_init(&attr);
 
-    pthread_create(&consumer, &attr, &consumer_loop, (void *) &r);
+        pthread_create(&publisher_th, &attr, &publisher_loop, (void *) &r);
 
-    intptr_t sent;
-    pthread_join(publisher, (void **) &sent);
+        pthread_create(&consumer_th, &attr, &consumer_loop, (void *) &r);
 
-    intptr_t recd;
-    pthread_join(consumer, (void **) &recd);
+        intptr_t sent;
+        pthread_join(publisher_th, (void **) &sent);
 
-    uint64_t end = get_time();
-    printf("\npublisher sent %ld messages\n", sent);
-    printf("consumer received %ld messages\n", recd);
+        intptr_t recd;
+        pthread_join(consumer_th, (void **) &recd);
 
-    pthread_attr_destroy(&attr);
+        uint64_t end = get_time();
+        time[i] = end - start;
 
-    queue_destroy(&r.q);
+        // for(size_t i = 0ULL; i < r.messages_per_thread; i++)
+            // printf("%lu\n", out[i]);
+
+        // printf("\npublisher sent %ld messages\n", sent);
+        // printf("consumer received %ld messages\n", recd);
+
+        pthread_attr_destroy(&attr);
+
+        queue_destroy(&r.q);
+    }
+    
+    qsort(time, 100U, sizeof(uint32_t), comp);
+    long long avg = 0LL;
+    for (int num = 16; num < 84; num++) {
+        avg += time[num];
+    }
+    avg /= 68;
+    // printf("average time : %lldus\n", avg);
+    printf("%lld\n", avg);
+
+    /*uint32_t time[100];
+    for (int i = 0; i < 100; i++) {
+            for(size_t i = 0; i < 65536ULL; i++) {
+            in[i] = i;
+            out[i] = 0ULL;
+        }
+        rbuf_t r;
+        r.num_threads = 1U;
+        if (argc > 1)
+            r.messages_per_thread = (uint32_t) atoi(argv[1]);
+        else
+            r.messages_per_thread = 65536;
+    
+        queue_init(&r.q, BUFFER_SIZE);
+
+        uint64_t start = get_time();
+
+        pthread_t publisher_th;
+        pthread_t consumer_th;
+
+        pthread_attr_t attr;
+        pthread_attr_init(&attr);
+
+        pthread_create(&publisher_th, &attr, &publisher_loop, (void *) &r);
+
+        pthread_create(&consumer_th, &attr, &consumer_loop, (void *) &r);
+
+        intptr_t sent;
+        pthread_join(publisher_th, (void **) &sent);
+
+        intptr_t recd;
+        pthread_join(consumer_th, (void **) &recd);
+
+        // for(size_t i = 0ULL; i < 65536ULL; i++)
+        // printf("%lu\n", out[i]);
+
+        // printf("\npublisher sent %ld messages\n", sent);
+        // printf("consumer received %ld messages\n", recd);
+
+        uint64_t end = get_time();
+        time[i] = end - start;
+
+        pthread_attr_destroy(&attr);
+
+        queue_destroy(&r.q);
+    }
+
+    qsort(time, 100U, sizeof(uint32_t), comp);
+    long long avg = 0LL;
+    for (int num = 16; num < 84; num++) {
+        avg += time[num];
+    }
+    avg /= 68;
+    // printf("average time : %lldus\n", avg);
+    printf("%lld\n", avg);*/
 
     return 0;
 }
