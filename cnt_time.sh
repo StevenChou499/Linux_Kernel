9b9656ef8b902d75e986038b17f3d7685e256ab8
StevenChou499 <stevenchou499@gmail.com>
2022-06-20 03:25:05 +0800

    Change for comparing buffer size.

--- cnt_time.sh
+++ cnt_time.sh
@@ -2,20 +2,39 @@
 
 #rm -f output_dyn
 #rm -f output
-rm -f output_msg_100
-rm -f output_algn
+#rm -f output_msg_100
+#rm -f output_algn
+#rm -f output_con
+rm -f output_b_con_100
+rm -f output_b_con_200
+rm -f output_b_con_400
+rm -f output_b_con_500
 
 #gcc -o test_dyn -pthread test_dyn.c
 #gcc -o test -pthread test.c
-gcc -o test_msg -pthread test_msg.c
-gcc -o test_algn -pthread test_algn.c
+#gcc -o test_msg -pthread test_msg.c
+#gcc -o test_algn -pthread test_algn.c
+gcc -o test_con_100 -pthread test_con_100.c
+gcc -o test_con_200 -pthread test_con_200.c
+gcc -o test_con_400 -pthread test_con_400.c
+gcc -o test_con_500 -pthread test_con_500.c
 
-for i in {128..65536..128};
+# for i in {128..65536..128};
+# do
+#     #./test_dyn $i >> output_dyn;
+#     #./test $i >> output;
+#     #./test_msg $i >> output_msg_100;
+#     ./test_algn m$i b4096 >> output_algn;
+#     ./test_con m$i b4096 >> output_con;
+# done
+
+for i in {4096..49152..4096};
 do
-    #./test_dyn $i >> output_dyn;
-    #./test $i >> output;
-    ./test_msg $i >> output_msg_100;
-    ./test_algn m$i >> output_algn;
+    #./test_algn b$i >> output_b_algn;
+    ./test_con_100 b$i >> output_b_con_100;
+    ./test_con_200 b$i >> output_b_con_200;
+    ./test_con_400 b$i >> output_b_con_400;
+    ./test_con_500 b$i >> output_b_con_500;
 done
 
 gnuplot compare.gp
--- compare.gp
+++ compare.gp
@@ -1,11 +1,12 @@
-set title "ring buffer speed"
-set xlabel "Number of messages (x128bytes)"
+set title "ring buffer speed (tranfer size = 65536bytes)"
+set xlabel "Size of ring buffer (x4096bytes)"
 set ylabel "time(usec)"
 set terminal png font " Times_New_Roman,12 "
-set output "tmp.png"
-set xtics 100 ,100 ,512
+set output "Compare_algn_con_bs_all.png"
 set key left 
 
-
-plot "output_msg_100" using 1 with linespoints linewidth 2 title "mmap with msg size 100", \
-"output_algn" using 1 with linespoints linewidth 2 title "mmap with msg size 100 and algn", \
+plot \
+"output_b_con_100" u ($0+1):1 with linespoints linewidth 2 title "mmap with msg size 100 and algn", \
+"output_b_con_200" u ($0+1):1 with linespoints linewidth 2 title "mmap with msg size 200 and algn", \
+"output_b_con_400" u ($0+1):1 with linespoints linewidth 2 title "mmap with msg size 400 and algn", \
+"output_b_con_500" u ($0+1):1 with linespoints linewidth 2 title "mmap with msg size 500 and algn", \
